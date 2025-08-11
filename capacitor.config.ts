e140c4e200a6ffd509c10a9b4ba37bdddb344bb9
Terrill Edwards <terrilledwards@icloud.com>
2025-08-10 20:44:51 -0400

    init commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'muser',
+  webDir: 'www'
+};
+
+export default config;
